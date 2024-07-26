0f129bb2d3adc166e5ed8849247158180fc441e0
DoyoungLee9720 <dlehdud224@gmail.com>
2024-07-26 10:32:45 +0900

    2024-07-26Test

--- java_bank_app/src/bank/app/BankApp.java
+++ java_bank_app/src/bank/app/BankApp.java
@@ -8,6 +8,7 @@ public class BankApp {
 	private static Scanner scanner = new Scanner(System.in);
 	private static List<Account> accounts = new ArrayList<>();
 	
+	//메인화면
 	public static void main(String[] args) {
 		boolean run = true;
 		while(run) {
@@ -33,8 +34,7 @@ public class BankApp {
 		System.out.println("프로그램 종료");
 	}
 	
-	
-	
+	//계좌생성 화면
 	private static void createAccount() {
 		System.out.println("--------------------계좌생성-------------------");
 		System.out.print("계좌번호 : ");
@@ -47,10 +47,13 @@ public class BankApp {
 		int balance = scanner.nextInt();
 		
 		System.out.println("결과: 계좌가 생성되었습니다.");
+		//계좌 새로 생성 및 추가
 		Account account = new Account(ano,owner,balance);
 		accounts.add(account);
 		scanner.nextLine();
 	}
+	
+	//계좌목록 출력 화면
 	private static void accountList() {
 		System.out.println("--------------------계좌목록-------------------");
 		
@@ -59,11 +62,13 @@ public class BankApp {
 		}
 	}
 	
+	//계좌 예끔 화면
 	private static void deposit() {
 		System.out.println("---------------------예금---------------------");
 		System.out.print("계좌번호 : ");
 		String ano = scanner.nextLine();
 		Account ac = findAccount(ano);
+		//계좌가 없을 경우
 		if(ac==null) {
 			System.out.println("결과: 계좌가 없습니다.");
 		}
@@ -73,23 +78,25 @@ public class BankApp {
 			System.out.println("결과: 예금이 생성되었습니다.");
 			int hap = ac.getBalance()+balance;
 			ac.setBalance(hap);
-			
 		}
 		scanner.nextLine();
-		
 	}
 	
+	//계좌 출금 화면
 	private static void withdraw() {
 		System.out.println("---------------------출금---------------------");
 		System.out.print("계좌번호 : ");
 		String ano = scanner.nextLine();
 		Account ac = findAccount(ano);
+		//계좌가 없을 경우
 		if(ac==null) {
 			System.out.println("결과: 계좌가 없습니다.");
 		}
 		else {
+			
 			System.out.print("출금액 : ");
 			int balance = scanner.nextInt();
+			//찾는 금액이 계좌에 있는 금액보다 높을 경우
 			if(ac.getBalance()<balance) {
 				System.out.println("결과 : 출금할 잔액이 부족합니다");
 			}
@@ -98,11 +105,11 @@ public class BankApp {
 				int hap = ac.getBalance()-balance;
 				ac.setBalance(hap);
 			}
-			
 		}
 		scanner.nextLine();
 	}
 	
+	//예금 출금시 계좌 찾은 기능
 	private static Account findAccount(String ano) {
 		for(Account A : accounts) {
 			if(A.getAno().equals(ano)) {
@@ -110,6 +117,5 @@ public class BankApp {
 			}
 		}
 		return null;
-		
 	}
 }
